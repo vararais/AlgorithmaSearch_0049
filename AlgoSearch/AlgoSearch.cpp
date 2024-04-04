7048c2f5e2d13b771b17c337438b7e21789672a0
vararais <varadigmaisnann@gmail.com>
2024-04-04 20:11:09 +0700

    Menambah Do

--- AlgoSearch/AlgoSearch.cpp
+++ AlgoSearch/AlgoSearch.cpp
@@ -28,6 +28,16 @@ void LinearSearch() {
     char ch;
     int ctr;
     int item;
+
+    do {
+        cout << "\nEnter the element you want to search: ";
+        cin >> item;
+
+        ctr = 0;
+        i = 0;
+
+
+    }
 }
 int main()
 {
