9c7277bb6ee05da3e308dfba8bef7d5deb8a19c2
vararais <varadigmaisnann@gmail.com>
2024-04-04 18:42:55 +0700

    Menambah While dan Jumlah Elemen Array

--- AlgoSearch/AlgoSearch.cpp
+++ AlgoSearch/AlgoSearch.cpp
@@ -5,7 +5,12 @@ int arr[20];
 int n;
 int i;
 
-void input ()
+void input() {
+    while (true) {
+        cout << "Enter the number of elements in the array :";
+
+    }
+}
 int main()
 {
     std::cout << "Hello World!\n";
