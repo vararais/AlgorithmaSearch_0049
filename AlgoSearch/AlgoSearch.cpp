cb5c864189f60b63e47f9ebb91b0878cd13b3577
vararais <varadigmaisnann@gmail.com>
2024-04-04 20:21:05 +0700

    Menambah Void Display

--- AlgoSearch/AlgoSearch.cpp
+++ AlgoSearch/AlgoSearch.cpp
@@ -49,6 +49,13 @@ void LinearSearch() {
         cin >> ch;
     } while ((ch == 'y') || (ch == 'Y'));
 }
+
+void display() {
+    cout << "Array elements are :";
+    for (i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
 int main()
 {
     std::cout << "Hello World!\n";
